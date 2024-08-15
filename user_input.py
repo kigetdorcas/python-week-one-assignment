3a672add5765ec0061743fcd71c35f5875b085cd
Kigetdorcas <dorcaskiget76@gmail.com>
2024-08-15 15:01:06 +0300

    python-week-one-assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
